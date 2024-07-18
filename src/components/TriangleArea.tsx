36c2ea59f7ecd48346aa7a44b5052101eb177cd3
matei-cozac <116168452+matei-cozac@users.noreply.github.com>
2024-07-18 16:24:00 +0300

    Update TriangleArea.tsx to deal with null cases

--- src/components/TriangleArea.tsx
+++ src/components/TriangleArea.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 interface TriangleAreaProps {
-    height: number;
-    width: number;
+    height: number | null;
+    width: number | null;
 }
 
 const TriangleArea: React.FC<TriangleAreaProps> = ({ height, width }) => {
+    if (height === null || width === null || height === 0 || width === 0) {
+        return null;
+    }
+
     const area = 0.5 * height * width;
     return (
         <div>
